a90b69223e34480ec71498fb2e167b1eb6152953
Gil Henrique <gilhenriquehb@gmail.com>
2021-05-06 12:19:47 -0300

    Implementação de HATEOAS de exemplo nas API

--- index.js
+++ index.js
@@ -76,8 +76,27 @@ var DB = {
 }
 
 app.get("/games",auth,(req, res) => {
+
+    var HATEOAS = [
+        {
+            href: "http://localhost:45679/game/0",
+            method: "DELETE",
+            rel: "delete_game"
+        },
+        {
+            href: "http://localhost:45679/game/0",
+            method: "GET",
+            rel: "get_game"
+        }, 
+        {
+            href: "http://localhost:45679/auth",
+            method: "POST",
+            rel: "login"
+        }
+    ]
+
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({ games: DB.games, _links: HATEOAS});
 });
 
 /*app.get("/games", auth, (req, res) => {
@@ -86,17 +105,36 @@ app.get("/games",auth,(req, res) => {
 });*/
 
 app.get("/game/:id",auth,(req, res) => {
+
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
         
         var id = parseInt(req.params.id);
 
+        var HATEOAS = [
+            {
+                href: "http://localhost:45679/game/" + id,
+                method: "DELETE",
+                rel: "delete_game"
+            },
+            {
+                href: "http://localhost:45679/game/" + id,
+                method: "PUT",
+                rel: "edit_game"
+            },
+            {
+                href: "http://localhost:45679/games",
+                method: "GET",
+                rel: "get_games"
+            }
+        ]
+
         var game = DB.games.find(g => g.id == id);
 
         if(game != undefined){
             res.statusCode = 200;
-            res.json(game);
+            res.json({game,_links:HATEOAS});
         }else{
             res.sendStatus(404);
         }
